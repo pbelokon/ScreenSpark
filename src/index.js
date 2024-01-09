8d9582d5a8a1e59735e5f960ad7ae65c494a101d
Pavel Belokon <pbelokon@myseneca.ca>
2024-01-09 16:34:05 -0500

    added propTypes to the star Component and added more custamizability

--- src/index.js
+++ src/index.js
@@ -1,13 +1,24 @@
-import React from "react";
+import React, { useState } from "react";
 import ReactDOM from "react-dom/client";
 // import "./index.css";
 // import App from "./App";
 import StarRating from "./StarRating";
+
+function Test() {
+  const [movieRating, setMovieRating] = useState(0);
+  return (
+    <div>
+      <StarRating maxRating={10} color="blue" onSetRating={setMovieRating} />
+      <p>This movies has rating of {movieRating}</p>
+    </div>
+  );
+}
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
     {/* <App /> */}
     <StarRating maxRating={10} />
+    <Test />
   </React.StrictMode>
 );
