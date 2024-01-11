defac9f02cdee153d9268f8309d609875d3dafd9
Pavel Belokon <pbelokon@myseneca.ca>
2024-01-10 20:22:35 -0500

    added api fetching and error handleling

--- src/index.js
+++ src/index.js
@@ -1,24 +1,12 @@
 import React, { useState } from "react";
 import ReactDOM from "react-dom/client";
-// import "./index.css";
-// import App from "./App";
+import "./index.css";
+import App from "./App";
 import StarRating from "./StarRating";
-
-function Test() {
-  const [movieRating, setMovieRating] = useState(0);
-  return (
-    <div>
-      <StarRating maxRating={10} color="blue" onSetRating={setMovieRating} />
-      <p>This movies has rating of {movieRating}</p>
-    </div>
-  );
-}
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
-    {/* <App /> */}
-    <StarRating maxRating={10} />
-    <Test />
-  </React.StrictMode>
+    <App />
+  </React.StrictMode> // would call call.rerender 2 times to make sure everything is correct
 );
